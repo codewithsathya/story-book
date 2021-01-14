8d892d3cb1b4e7d3898320cda7be1ce5b8200f91
S Sathiya Seelan <sss18@iitbbs.ac.in>
2021-01-14 15:45:56 +0530

    added google auth with passportjs

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ const router = express.Router();
 // Login/Landing page
 // @route GET /
 router.get("/", (req, res) => {
-  res.render("login");
+  res.render("login", {
+      layout: 'login'
+  });
 });
 
 // Dashboard
